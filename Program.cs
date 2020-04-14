eb4cafe3dab73fa6155c34a81a0b66de69d69577
Felipe <fraifelipe@gmail.com>
2020-04-13 21:19:19 -0300

    is that

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ProblemaCores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var blueWhiteRed = 100;
+
+
+            while (true)
+            {
+                var blue = blueWhiteRed.ToString()[0];
+                var white = blueWhiteRed.ToString()[1];
+                var red = blueWhiteRed.ToString()[2];
+
+                var result = blueWhiteRed * 3;
+
+                if (result.ToString()[0] == red && result.ToString()[1] == red && result.ToString()[2] == red)
+                {
+                    break;
+                }
+
+                blueWhiteRed++;
+            }
+
+
+            Console.WriteLine("blueWhiteRed is " + blueWhiteRed);
+            Console.WriteLine("Red is " + blueWhiteRed.ToString()[2]);
+            Console.WriteLine("Result is " + blueWhiteRed * 3);
+        }
+    }
+}
